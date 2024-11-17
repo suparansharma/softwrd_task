33fd3ac0b6e139b3d4156a80c1a2ce1259007b33
suparansharma <suparansharmap@gmail.com>
2024-11-18 00:05:54 +0600

    header added

--- src/lib/TableElement.svelte
+++ src/lib/TableElement.svelte
@@ -1,11 +1,117 @@
 <script>
-let { landpads } = $props();
-$inspect('inspect landpads', landpads);
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell,
+		Button,
+		Modal,
+		Progressbar,
+		Badge
+	} from 'flowbite-svelte';
+	import { Dropdown, DropdownItem } from 'flowbite-svelte';
+	import { ChevronDownOutline } from 'flowbite-svelte-icons';
+
+	let { landpads } = $props();
+
+	let defaultModal = $state(false);
+	let landpadDetails = $state('');
+	let selectedStatus = $state('');
+
+	// Helper function to calculate the success rate
+	const calculateSuccessRate = (successful, attempted) => {
+		return attempted > 0 ? ((successful / attempted) * 100).toFixed(2) : 'N/A';
+	};
+
+	const handelDetailsModal = (landpad) => {
+		defaultModal = true;
+		landpadDetails = landpad?.details;
+	};
+
+	const getBadgeColor = (status) => {
+		if (status === 'active') return 'green';
+		if (status === 'retired') return 'red';
+		if (status === 'under construction') return 'indigo';
+		return 'dark'; // Default color if no match
+	};
+
+	const filteredLandpads = () =>
+		selectedStatus ? landpads.filter((landpad) => landpad.status === selectedStatus) : landpads;
 </script>
 
+<Button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+	Dropdown button
+	<ChevronDownOutline class="w-6 h-6 ms-2 text-white dark:text-white" />
+</Button>
 
-<div>
+<Dropdown>
+	<DropdownItem on:click={() => (selectedStatus = '')}>All</DropdownItem>
+	<DropdownItem on:click={() => (selectedStatus = 'active')}>Active</DropdownItem>
+	<DropdownItem on:click={() => (selectedStatus = 'retired')}>Retired</DropdownItem>
+	<DropdownItem on:click={() => (selectedStatus = 'under construction')}>
+		Under Construction
+	</DropdownItem>
+</Dropdown>
 
-    
-  
-</div>
+<Table hoverable={true}>
+	<TableHead>
+		<TableHeadCell>Full Name</TableHeadCell>
+		<TableHeadCell>Location Name</TableHeadCell>
+		<TableHeadCell>Success Rate (%)</TableHeadCell>
+		<TableHeadCell>Wikipedia Link</TableHeadCell>
+		<TableHeadCell>Details</TableHeadCell>
+		<TableHeadCell>Status</TableHeadCell>
+	</TableHead>
+	<TableBody tableBodyClass="divide-y">
+		{#each filteredLandpads() as landpad}
+			<TableBodyRow>
+				<TableBodyCell>{landpad.full_name}</TableBodyCell>
+				<TableBodyCell>
+					{landpad.location.name}, {landpad.location.region}
+				</TableBodyCell>
+				<TableBodyCell>
+					<div class="my-4">
+						<Progressbar
+							progress={calculateSuccessRate(
+								landpad.successful_landings,
+								landpad.attempted_landings
+							)}
+							color="green"
+						/>
+						<div class="mb-1 text-base font-medium dark:text-white">
+							{calculateSuccessRate(landpad.successful_landings, landpad.attempted_landings)}%
+						</div>
+					</div>
+				</TableBodyCell>
+				<TableBodyCell>
+					<a
+						href={landpad.wikipedia}
+						class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn More
+					</a>
+				</TableBodyCell>
+				<TableBodyCell>
+					<button on:click={() => handelDetailsModal(landpad)}>Details</button>
+				</TableBodyCell>
+				<TableBodyCell class="capitalize">
+					<Badge color={getBadgeColor(landpad.status)}>{landpad.status}</Badge>
+				</TableBodyCell>
+			</TableBodyRow>
+		{/each}
+	</TableBody>
+</Table>
+<Modal title="Terms of Service" bind:open={defaultModal} autoclose>
+	<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+		{landpadDetails}
+	</p>
+
+	<svelte:fragment slot="footer">
+		<Button on:click={() => alert('Handle "success"')}>I accept</Button>
+		<Button color="alternative">Decline</Button>
+	</svelte:fragment>
+</Modal>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,11 +21,20 @@
 	});
 </script>
 
+<div class="w-full pt-10 pb-10 text-center border-b-4 item-center mb-10">
+	<img
+		class="h-[50px] mx-auto"
+		src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/SpaceX_Logo_Black.png/797px-SpaceX_Logo_Black.png"
+		alt=""
+		srcset=""
+	/>
+</div>
+
 <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
 	<div class="col-span-1 lg:col-span-2 border-r pr-4">
-        <TableElement {landpads}/>
-    </div>
+		<TableElement {landpads} />
+	</div>
 	<div class="col-span-1 pl-4">
-		<MapElement landpads={landpads} />
+		<MapElement {landpads} />
 	</div>
 </div>
